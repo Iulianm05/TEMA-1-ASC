4236b7bc8f4cf9cab684e30b1899843a470ccf92
Iulianm05 <103935302+Iulianm05@users.noreply.github.com>
2022-04-18 14:39:49 +0300

    Add files via upload

---
+++ tema1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1;
+    scanf("%d",&num1);
+    if(num1==1 || num1==6 || num1==4 || num1==7)
+        printf("Cifra se afla in multimea {1,6,4,7}");
+    else 
+        printf("Cifra nu se afla in multime");
+
+}
